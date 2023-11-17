62e7548444fc26b1b25f0567040e9bbf01e071d5
aleksandrgenrikhs <aleksandr.genrikhs@gmail.com>
2023-11-17 17:39:08 +0300

    add MVP General Tab

---
+++ headlinesfeature/src/main/java/com/genrikhsalexandr/headlinesfeature/presentation/presenter/GeneralPresenter.java
@@ -0,0 +1,63 @@
+package com.genrikhsalexandr.headlinesfeature.presentation.presenter;
+
+
+import androidx.fragment.app.FragmentManager;
+
+import com.genrikhsaleksandr.core.domain.Category;
+import com.genrikhsaleksandr.core.domain.model.Article;
+import com.genrikhsaleksandr.core.navigation.Navigator;
+import com.genrikhsaleksandr.core.presentation.ArticleItemList;
+import com.genrikhsalexandr.headlinesfeature.domain.HeadlinesInteractor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import moxy.InjectViewState;
+import moxy.MvpPresenter;
+
+@InjectViewState
+public class GeneralPresenter extends MvpPresenter<BusinessView> {
+
+    Navigator navigator;
+
+    HeadlinesInteractor interactor;
+
+    @Inject
+    public GeneralPresenter(
+            HeadlinesInteractor interactor,
+            Navigator navigator
+    ) {
+        this.interactor = interactor;
+        this.navigator = navigator;
+        getViewState().setLoading(true);
+        interactor.getArticlesList(Category.GENERAL)
+                .subscribe(this::onArticlesLoaded, this::onError);
+    }
+
+    private void onError(Throwable throwable) {
+        String errorMessage = "An error occurred: " + throwable.getMessage();
+        System.err.println(errorMessage);
+    }
+
+    private void onArticlesLoaded(List<Article> articles) {
+        List<ArticleItemList> articlesItems = new ArrayList<>(articles.size());
+        articles.forEach(article -> articlesItems.add(
+                new ArticleItemList(
+                        article.getSourceName(),
+                        article.getTitle(),
+                        article.getUrlToImage(),
+                        article,
+                        article.getId()
+                )
+        ));
+        System.out.println("articles = " + articles.size());
+        getViewState().setLoading(false);
+        getViewState().showArticles(articlesItems);
+    }
+
+    public void onNewsItemClick(Article article, FragmentManager fragmentManager) {
+        navigator.navigateToDetailsArticle(article, fragmentManager);
+    }
+}
--- headlinesfeature/src/main/java/com/genrikhsalexandr/headlinesfeature/presentation/tabs/GeneralFragment.java
+++ headlinesfeature/src/main/java/com/genrikhsalexandr/headlinesfeature/presentation/tabs/GeneralFragment.java
@@ -8,24 +8,31 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DividerItemDecoration;
 
+import com.genrikhsaleksandr.core.presentation.ArticleItemList;
+import com.genrikhsaleksandr.core.presentation.CoreAdapter;
 import com.genrikhsalexandr.headlinesfeature.databinding.FragmentGeneralBinding;
 import com.genrikhsalexandr.headlinesfeature.di.HeadlinesComponentProvider;
-import com.genrikhsalexandr.headlinesfeature.presentation.adapter.HeadlinesAdapter;
+import com.genrikhsalexandr.headlinesfeature.presentation.presenter.BusinessView;
+import com.genrikhsalexandr.headlinesfeature.presentation.presenter.GeneralPresenter;
 
-import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import java.util.List;
 
-public class GeneralFragment extends Fragment {
+import javax.inject.Inject;
 
-    public static GeneralFragment newInstance() {
-        GeneralFragment fragment = new GeneralFragment();
-        return fragment;
+import kotlin.Unit;
+import moxy.MvpAppCompatFragment;
+
+
+public class GeneralFragment extends MvpAppCompatFragment implements BusinessView {
+
+    public GeneralFragment() {
     }
 
-    private final CompositeDisposable disposables = new CompositeDisposable();
-
+    public static GeneralFragment newInstance() {
+        return new GeneralFragment();
+    }
 
     private FragmentGeneralBinding _binding;
 
@@ -33,6 +40,24 @@ public class GeneralFragment extends Fragment {
         return _binding;
     }
 
+    @Inject
+    GeneralPresenter presenter;
+
+    CoreAdapter adapter;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        ((HeadlinesComponentProvider) requireActivity().getApplication())
+                .provideHeadlinesComponent()
+                .inject(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        presenter.attachView(this);
+    }
 
     @Nullable
     @Override
@@ -43,12 +68,30 @@ public class GeneralFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        adapter = new CoreAdapter(article ->
+        {
+            presenter.onNewsItemClick(article, getParentFragmentManager());
+            return Unit.INSTANCE;
+        });
+        getBinding().rvGeneral.setAdapter(adapter);
         getBinding().rvGeneral.addItemDecoration(
                 new DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL)
         );
-      //  getBinding().rvGeneral.setAdapter(adapter);
+    }
+
+    @Override
+    public void setLoading(Boolean isLoading) {
+        System.out.println("isLoading = " + isLoading);
+        getBinding().progressBar.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+    }
+
+    @Override
+    public void showArticles(List<ArticleItemList> news) {
+        getBinding().rvGeneral.setVisibility(View.VISIBLE);
+        adapter.submitData(news);
+        System.out.println("showArticles = " + news);
     }
 
     @Override
